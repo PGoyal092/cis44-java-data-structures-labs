722bed328b1e9e8027682a7cc9cd849d4b57316d
PGoyal092 <pgoyal4@mywvm.wvm.edu>
2025-10-31 20:38:22 -0700

    Add files via upload

---
+++ lab9-arithmetic_tree/BinaryTreeNode.java
@@ -0,0 +1,30 @@
+public class BinaryTreeNode {
+    String value;
+    BinaryTreeNode parent;
+    BinaryTreeNode left;
+    BinaryTreeNode right;
+
+    public BinaryTreeNode(String value) {
+        this.value = value;
+        this.parent = null;
+        this.left = null;
+        this.right = null;
+    }
+    public void traversePreorder() {
+        System.out.print(value+" ");
+        if(left!=null) left.traversePreorder();
+        if(right!=null) right.traversePreorder();
+    }
+    public void traverseInorder() {
+        System.out.print("(");
+        if(left!=null) left.traverseInorder();
+        System.out.print(" "+value+" ");
+        if(right!=null) right.traverseInorder();
+        System.out.print(")");
+    }
+    public void traversePostorder() {
+        if(left!=null) left.traversePostorder();
+        if(right!=null) right.traversePostorder();
+        System.out.print(value+" ");
+    }
+}
